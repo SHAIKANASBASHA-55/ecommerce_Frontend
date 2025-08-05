277148382a0e54b458e70b71bd96cee80ca6a8c6
SHAIKANASBASHA-55 <2300032367@kluniversity.in>
2025-08-05 12:48:39 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ This makes assets load with relative paths
-})
+  base: '/ecommerce/', // ✅ Required for Apache in /ecommerce/
+});
